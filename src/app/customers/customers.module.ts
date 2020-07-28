d99b80c54c83adf8c76d95f3ffe0c716aeed681a
xcellent-dev <wangjin8855@outlook.com>
2020-07-28 23:25:38 +0800

    add: customers module created

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }      from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersComponent }  from './customers.component';
+
+@NgModule({
+  imports:      [ CommonModule ],
+  declarations: [ CustomersComponent ]
+})
+export class CustomersModule { }
