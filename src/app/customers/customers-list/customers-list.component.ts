7eb88dacec85f5a2c7463f95f42972b63482cdc5
xcellent-dev <wangjin8855@outlook.com>
2020-07-29 00:58:25 +0800

    update: customer-list module

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -7,14 +7,27 @@ import {ICustomer} from "../../shared/interfaces";
   styleUrls: ['./customers-list.component.css']
 })
 export class CustomersListComponent implements OnInit {
-  filertedCusomters: ICustomer[] = [];
+  filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredCustomers = [
+      { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
+      { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
+      { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
+      { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
+    ];
 
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((cust: ICustomer) => {
+      this.customersOrderTotal += cust.orderTotal;
+    });
   }
 
 }
